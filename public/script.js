61e869adcc14fadf1af9e4e5feae191bcdbdea47
PirateNahid <piratenahid46@gmail.com>
2022-10-18 23:04:48 +0600

    kk

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log("from script file");
+function revealTBZ(){
+    const hiddenTBZ = document.getElementById("hiddenTBZ");
+    const revealTBZ = document.getElementById("revealTBZ");
+    if(hiddenTBZ.style.display == "block"){
+        hiddenTBZ.style.display = "none";
+        revealTBZ.innerHTML = "Read More";
+
+    }else{
+        hiddenTBZ.style.display = "block";
+        revealTBZ.innerHTML = "See Less";
+    }
+}
